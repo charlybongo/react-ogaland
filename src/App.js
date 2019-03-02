fc0c1646648e974f29f14a65dbfd766f400213f0
charlybongo <charlespheonix@gmail.com>
2019-03-02 02:50:10 +0100

    rental list

--- src/App.js
+++ src/App.js
@@ -1,26 +1,21 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import {BrowserRouter, Route} from 'react-router-dom';
+import { Header } from "./shared/Header";
+import { RentalList } from "./components/rental/RentalList";
+import { RentalDetail } from "./components/rental/RentalDetail";
+import "./App.scss";
 
 class App extends Component {
   render() {
     return (
+      <BrowserRouter>
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Header />
+        <div className="container" />
+       <Route exact path="/" component={RentalList}/>
+       <Route exact path="/test" component={RentalDetail}/>
       </div>
+      </BrowserRouter>
     );
   }
 }
---
+++ src/components/rental/RentalDetail.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export class RentalDetail extends React.Component {
+
+    render() {
+        return (
+            <div>
+
+                <h1>i am detail component</h1>
+            </div>
+        )
+    }
+}
